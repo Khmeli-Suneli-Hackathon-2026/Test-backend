f5b407450a523e04ab8ab904fba2822e0fe3bcf9
HoshkoElizabeth <liza.hoshko.v@gmail.com>
2026-04-29 02:13:03 +0300

    chore: setup drizzle orm with mysql2

---
+++ src/services/db.js
@@ -0,0 +1,9 @@
+import { drizzle } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+
+
+const pool = mysql.createPool(process.env.DATABASE_URL);
+
+export const db = drizzle(pool);
+
+console.log("Drizzle initialised for MySQL");

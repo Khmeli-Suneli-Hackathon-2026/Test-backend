15fe64931d1c3488e05da1b89b6792188720770e
HoshkoElizabeth <liza.hoshko.v@gmail.com>
2026-04-29 01:34:41 +0300

    feat: implement global error handling middleware

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import errorHandler from './src/middlewares/errorMiddleware.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,6 +10,14 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('API працює!');
 });
+
+app.get('/error', (req, res, next) => {
+    const err = new Error('Smth went wrong!');
+    res.status(400);
+    next(err); 
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port:${PORT}`);

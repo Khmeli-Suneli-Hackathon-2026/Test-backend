8a67450dd90028d3a468bb07916e415256e888b4
HoshkoElizabeth <liza.hoshko.v@gmail.com>
2026-04-30 00:15:56 +0300

    feat: connect auth routes to the main app

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 import errorHandler from './src/middlewares/errorMiddleware.js';
+import register_route from './src/routes/auth/register_route.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api/auth', register_route);
 
 app.get('/', (req, res) => {
     res.send('API працює!');
